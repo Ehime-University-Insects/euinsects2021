8de4a3e916f03d16877669e595960d117df1c95e
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2021-07-07 11:16:11 +0900

    #3 対応 archive.php を category.php に複製

--- functions.php
+++ functions.php
@@ -2,10 +2,10 @@
 
 function twentytwentyone_styles()
 {
-  wp_enqueue_style(
-    'parent',
-    get_template_directory_uri() . '/style.css'
-  );
+    wp_enqueue_style(
+        'parent',
+        get_template_directory_uri() . '/style.css'
+    );
 }
 add_action('wp_enqueue_scripts', 'twentytwentyone_styles');
 
