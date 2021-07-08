466113ba5f9cd940939035616392b7ed0d484a5d
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2021-07-08 09:34:39 +0900

    #7 PHP_INT_MAX

--- functions.php
+++ functions.php
@@ -1,13 +1,23 @@
 <?php
 
-function twentytwentyone_styles()
+function my_theme_enqueue_styles()
 {
+    $parenthandle = 'parent-style';
+    $theme        = wp_get_theme();
     wp_enqueue_style(
-        'parent',
-        get_template_directory_uri() . '/style.css'
+        $parenthandle,
+        get_template_directory_uri() . '/style.css',
+        array(),
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version')
     );
 }
-add_action('wp_enqueue_scripts', 'twentytwentyone_styles');
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles', PHP_INT_MAX);
 
 // /** Category list
 //  */
