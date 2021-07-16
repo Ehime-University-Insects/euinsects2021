8f6c4144c2f45b584ab3890dd954e95ee0803305
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2021-07-16 09:06:56 +0900

    不要コメントの削除

--- functions.php
+++ functions.php
@@ -26,27 +26,6 @@ add_filter('get_the_archive_title', function ($title) {
     return $title;
 });
 
-// /** Category list
-//  */
-// function get_category_titles( $cat, $showposts = 5 ) {
-// 	$args = array(
-// 		'cat' => $cat,
-// 		'post_type' => 'post',
-// 		'showposts' => $showposts,
-// 	);
-// 	$the_query = new WP_Query( $args );
-
-// 	if ( $the_query->have_posts() ) {
-// 		echo '<ul>';
-// 		while ( $the_query->have_posts() ) {
-// 			$the_query->the_post();
-// 			echo '<li>' . get_the_title() . '</li>';
-// 		}
-// 		echo '</ul>';
-// 		wp_reset_postdata();
-// 	}
-// }
-
 /** Google fonts
  */
 function euinsects_custom_fonts() {
@@ -56,21 +35,3 @@ function euinsects_custom_fonts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'euinsects_custom_fonts' );
-
-// /** Custom colors */
-// function twentytwenty_accent_accessible_colors_fallback( $value ) {
-// 	$accent_hue_active = get_theme_mod( 'accent_hue_active', 'default' );
-// 	$background_color  = sanitize_hex_color_no_hash( get_theme_mod( 'background_color' ) );
-// 	$value             = (array) $value;
-
-// 	// if ( 'default' === $accent_hue_active && 'f5efe0' === $background_color ) {
-// 		$value['content'] = array(
-// 			'text'      => '#000000',
-// 			'accent'    => '#549B35',
-// 			'secondary' => '#6d6d6d',
-// 			'borders'   => '#dcd7ca',
-// 		);
-// 	// }
-// 	return $value;
-// }
-// add_filter( 'theme_mod_accent_accessible_colors', 'twentytwenty_accent_accessible_colors_fallback' );
